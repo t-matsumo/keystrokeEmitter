5408f0dce12f2075789f2f8038c932d6d957faf4
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2016-09-19 21:54:36 +0900

    delete /view/signin/error.ejs

--- server/routes/signin.js
+++ server/routes/signin.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('signin/signin', { title: 'Express' });
+  res.render('signin', { title: 'Express' });
 });
 
 router.post('/', function(req, res, next) {
   if (false) {
     // res.render('サインイン後', { title: 'Express', error: true });
   } else {
-    res.render('signin/signin', { title: 'Express', error: true });
+    res.render('signin', { title: 'Express', error: true });
   }
 });
 
