5cb10f909bd7e468bf713882fd349a7d37d43638
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2016-09-19 16:13:37 +0900

    add code in error at signin.ejs

--- server/routes/signin.js
+++ server/routes/signin.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET page. */
 router.get('/', function(req, res, next) {
-  res.render('signin', { title: 'Express' });
+  res.render('signin/signin', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  if (false) {
+    // res.render('サインイン後', { title: 'Express', error: true });
+  } else {
+    res.render('signin/signin', { title: 'Express', error: true });
+  }
 });
 
 module.exports = router;
