0dc63a32acc167835c03a21a2f3fd0b64acef08d
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2016-09-27 03:01:43 +0900

    implement WebSocket

--- server/routes/content.js
+++ server/routes/content.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('content', { title: 'Express' });
+  res.render('content', { title: 'contents' });
 });
 
 module.exports = router;
