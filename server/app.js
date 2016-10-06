df7f824c48797ef6363fc42d3d744960ba42aebc
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2016-10-06 15:38:44 +0900

    implements the three.js

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/three/build')));
 
 app.use('/', routes);
 app.use('/signin', signin);
